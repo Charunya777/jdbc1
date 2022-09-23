8f474f4dd8020989437d03d538c27ed7a32d14b4
Charunya777 <charunyajavadeveloper@gmail.com>
2022-09-23 22:05:25 +0530

    DTO of User

---
+++ User.java
@@ -0,0 +1,54 @@
+package com.ty.dto;
+
+public class User {
+	private String firstname;
+	private String Lastname;
+	private String email;
+	private String password;
+	private long phone;
+	private String dateofbirth;
+	private String gender;
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return Lastname;
+	}
+	public void setLastname(String lastname) {
+		Lastname = lastname;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public long getPhone() {
+		return phone;
+	}
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+	public String getDateofbirth() {
+		return dateofbirth;
+	}
+	public void setDateofbirth(String dateofbirth) {
+		this.dateofbirth = dateofbirth;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+}
